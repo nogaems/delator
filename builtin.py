46e783c517e208d891109f0858847599fdb7a8bb
nogaems <nogaems@users.noreply.github.com>
2020-06-23 20:21:34 +0300

    add youtube_dl title extraction method
    Use this by default and if it doesn't work out then fallback
    to fetching urls manually and extracting titles from that.

--- builtin.py
+++ builtin.py
@@ -1,11 +1,24 @@
 import asyncio
 import magic
 import chardet
+import youtube_dl
 
 import re
 import gzip
 
 import config as cfg
+
+
+class CustomLogger:
+    def debug(self, msg):
+        pass
+
+    def warning(self, msg):
+        pass
+
+    def error(self, msg):
+        pass
+
 
 class MessageLinksInfo:
     chunk_size = 100000
@@ -14,6 +27,10 @@ class MessageLinksInfo:
     def __init__(self, session):
         self.session = session
         self.magic = magic.Magic(mime=True, uncompress=True)
+        self.ytdl = youtube_dl.YoutubeDL({
+            'skip_download': True,
+            'logger': CustomLogger()
+        })
 
     def _parse_urls(self, message):
         regex = r'http[s]?:\/\/(?:[a-zA-Z]|[0-9]|[$-_~@.&+]|[!*\(\),]|(?:%[0-9a-fA-F][0-9a-fA-F]))+'
@@ -48,40 +65,53 @@ class MessageLinksInfo:
         return match[0] if match else match
 
     async def _fetch(self, session, urls):
-        chunks = []
+        entities = []
         for url in urls:
-            try:
-                async with session.get(url, proxy=cfg.proxy if hasattr(cfg, 'proxy') else None) as response:
-                    chunk = await response.content.readany()
-                    while hasattr(response.connection, 'closed') and\
-                            not response.connection.closed and \
-                            len(chunk) < self.chunk_size:
-                        chunk += await response.content.readany()
-                    chunks.append(chunk)
-            except Exception as e:
-                chunks.append(e)
-        return chunks
+            title = self._ytdl_extract_title(url)
+            if title:
+                entities.append(title)
+            else:
+                try:
+                    async with session.get(url, proxy=cfg.proxy if hasattr(cfg, 'proxy') else None) as response:
+                        chunk = await response.content.readany()
+                        while hasattr(response.connection, 'closed') and \
+                                not response.connection.closed and \
+                                len(chunk) < self.chunk_size:
+                            chunk += await response.content.readany()
+                        entities.append(chunk)
+                except Exception as e:
+                    entities.append(e)
+        return entities
+
+    def _ytdl_extract_title(self, url):
+        try:
+            extracted = self.ytdl.extract_info(url)
+            return extracted['title']
+        except Exception as error:
+            return None
 
     async def _get_info(self, message):
         urls = self._parse_urls(message)
-        chunks = None
+        entities = None
         if urls:
-            chunks = await self._fetch(self.session, urls)
-        if chunks:
+            entities = await self._fetch(self.session, urls)
+        if entities:
             info = []
-            for chunk in chunks:
-                if not isinstance(chunk, bytes):
-                    info.append(f'Bad link: {repr(chunk)}')
-                else:
-                    dcomp = self._decompress(chunk)
-                    chunk = dcomp if dcomp else chunk
+            for entity in entities:
+                if isinstance(entity, str):
+                    info.append(f'Title: {entity}')
+                elif isinstance(entity, bytes):
+                    dcomp = self._decompress(entity)
+                    entity = dcomp if dcomp else entity
 
-                    title = self._parse_title(chunk)
+                    title = self._parse_title(entity)
                     if isinstance(title, str):
                         info.append(f'Title: {title}')
                     else:
-                        file_type = self.magic.from_buffer(chunk)
+                        file_type = self.magic.from_buffer(entity)
                         info.append(f'File type: {file_type}')
+                else:
+                    info.append(f'Bad link: {repr(entity)}')
             return info
 
     def get_info(self, message):
