7044c55f58c290ab3a125fb89239319398d2ff3e
nogaems <nogaems@users.noreply.github.com>
2020-06-28 18:02:50 +0300

    dice: truncate output

--- commands/dice.py
+++ commands/dice.py
@@ -3,7 +3,10 @@ import dice
 
 name = 'dice'
 aliases = ('d', 'в', 'вшсу')
-help = 'Roll a die using dice notation (https://github.com/borntyping/python-dice#notation)'
+help = '''Roll a die using dice notation (https://github.com/borntyping/python-dice#notation)
+In order to prevent chat from being wiped by rolling a large amount of dices (by mistake or on purpose), the output is truncated to be up to 1024 symbols in length.'''
+
+max_characters = 1024
 
 
 async def handler(args, request):
@@ -16,6 +19,8 @@ async def handler(args, request):
         result = dice.utilities.verbose_print(die)
         lines = result.split('\n')
         result = ''.join(map(lambda s: s.strip(), lines))
+        if len(result) > max_characters:
+            result = result[:max_characters - 1] + '…'
     except Exception as e:
         result = str(e)
     await request.reply(result)
